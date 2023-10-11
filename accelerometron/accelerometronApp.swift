805a421dfb0603b4bfd0cfcb6daf53cadd1b8f94
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-11 08:49:40 -0400

    Initial Commit

---
+++ accelerometron/accelerometronApp.swift
@@ -0,0 +1,17 @@
+//
+//  accelerometronApp.swift
+//  accelerometron
+//
+//  Created by Jimmy Ghelani on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct accelerometronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
